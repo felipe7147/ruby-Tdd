d726f84fcbf63e58beaf333b8d7cbd6c7a85bb28
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-20 11:35:34 +0000

    Subject

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -3,19 +3,16 @@ require 'calculator'
 describe Calculator do
 	context'#sum' do 
   it 'with positive numberss' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
+    result = subject.sum(5, 7)
     expect(result).to eq(12)
   end
 
   it 'with negative and positive numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, 7)
+    result = subject.sum(-5, 7)
     expect(result).to eq(2)
 end
  it 'with negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, -7)
+    result = subject.sum(-5, -7)
     expect(result).to eq(-12)
 end
   end
